7323476781cfde69feafb763f3fdfc5c3009b389
Edinsson Melo <emelo@bancolombia.com.co>
2025-07-16 23:14:29 -0500

    fix config gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-const repo = 'agencia-latam-landing';                    // 👈 nombre del repositorio
-const isGhPages = process.env.GITHUB_ACTIONS === 'true'; // se define true en CI
-
 const nextConfig = {
   output: 'export',
-  basePath: isGhPages ? `/${repo}` : '',
-  assetPrefix: isGhPages ? `/${repo}/` : '',
   trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
@@ -15,10 +10,6 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
-  },
-  // Configuración para variables de entorno
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isGhPages ? `/${repo}` : '',
   },
 }
 
